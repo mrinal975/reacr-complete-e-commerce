66ef30cfed532041002cc7682155b2812c5ade62
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-25 17:19:34 +0600

    update cotext

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import MyContext from "./myContext";
 
 function MyState(props) {
-  const state = {
-    name: "Atrika",
-    study: "One C",
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17,24,39)";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
   };
   return (
-    <MyContext.Provider value={state}>{props.children}</MyContext.Provider>
+    <MyContext.Provider value={toggleMode}>{props.children}</MyContext.Provider>
   );
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,12 +4,10 @@ import MyContext from "../../context/data/myContext";
 
 function Home() {
   const context = useContext(MyContext);
-  const { name, study } = context;
   return (
     <>
       <Layout>
-        <h1>Name: {name}</h1>
-        <h1>Age: {study}</h1>
+        <h1>OOp</h1>
       </Layout>
     </>
   );
