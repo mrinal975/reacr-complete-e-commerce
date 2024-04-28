ac81cca8de150b8fc79fdd02e3a034cbb8d26088
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-28 13:00:43 +0600

    category section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import Layout from "../../components/layout/Layout";
 import MyContext from "../../context/data/myContext";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Category from "../../components/category/Category";
 
 function Home() {
   const context = useContext(MyContext);
   return (
     <>
       <Layout>
-        <h1>OOp</h1>
+        <HeroSection />
+        <Category />
       </Layout>
     </>
   );
