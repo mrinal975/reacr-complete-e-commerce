090832ad5297619b5e0f8d6156cfd7ebdc5b6760
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-29 18:24:39 +0600

    reducer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,12 +7,26 @@ import ProductCard from "../../components/productCard/ProductCard";
 import Track from "../../components/track/Track";
 import Testimonial from "../../components/testimonial/Testimonial";
 import Footer from "../../components/footer/Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, deleteFromCart } from "../../redux/cartSlice";
 
 function Home() {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart);
+  const addCart = () => {
+    dispatch(addToCart("Shirt"));
+  };
+  const deleteCart = () => {
+    dispatch(deleteFromCart("Shirt"));
+  };
   const context = useContext(MyContext);
   return (
     <>
       <Layout>
+        <div className="flex gap-5 justify-center">
+          <button onClick={addCart}>Add</button>
+          <button onClick={addCart}>Remove</button>
+        </div>
         <HeroSection />
         <Filter />
         <ProductCard />
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartSlice,
+  },
   devTools: true,
 });
