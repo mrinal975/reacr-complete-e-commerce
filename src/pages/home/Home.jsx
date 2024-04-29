146908213bca925a2967d728169540678151530f
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-29 13:16:05 +0600

    filter component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Layout from "../../components/layout/Layout";
 import MyContext from "../../context/data/myContext";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Category from "../../components/category/Category";
+import Filter from "../../components/filter/Filter";
 
 function Home() {
   const context = useContext(MyContext);
@@ -11,6 +12,7 @@ function Home() {
       <Layout>
         <HeroSection />
         <Category />
+        <Filter />
       </Layout>
     </>
   );
