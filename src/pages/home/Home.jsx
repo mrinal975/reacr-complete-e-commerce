d093bb021338ff65bd49783e3a28859e00497bbe
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-29 13:24:56 +0600

    testimonial and track

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ import MyContext from "../../context/data/myContext";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Category from "../../components/category/Category";
 import Filter from "../../components/filter/Filter";
+import ProductCard from "../../components/productCard/ProductCard";
+import Track from "../../components/track/Track";
+import Testimonial from "../../components/testimonial/Testimonial";
 
 function Home() {
   const context = useContext(MyContext);
@@ -13,6 +16,9 @@ function Home() {
         <HeroSection />
         <Category />
         <Filter />
+        <ProductCard />
+        <Track />
+        <Testimonial />
       </Layout>
     </>
   );
